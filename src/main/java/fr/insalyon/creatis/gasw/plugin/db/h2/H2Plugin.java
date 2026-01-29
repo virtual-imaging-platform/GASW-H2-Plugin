7ce39a5975d12783da577e90c355071cbf070456
Ethaniel Billon <ebillon@student.42lyon.fr>
2026-01-29 09:51:02 +0100

    update h2 and java version
    - migrate from Java11 to Java21
    - migrate from log4j to slf4j
    - migrate from h2 1.x to 2.x

--- src/main/java/fr/insalyon/creatis/gasw/plugin/db/h2/H2Plugin.java
+++ src/main/java/fr/insalyon/creatis/gasw/plugin/db/h2/H2Plugin.java
@@ -75,7 +75,7 @@ public class H2Plugin implements DatabasePlugin {
             return "jdbc:h2:tcp://" + conf.getServerHost() + ":" 
                     + conf.getServerPort() + "/" 
                     + GaswConfiguration.getInstance().getExecutionPath() 
-                    + "/"+ conf.getDbPath() + ";create=true;MVCC=TRUE";
+                    + "/"+ conf.getDbPath() + ";IFEXISTS=FALSE";
         } else {
             return "jdbc:h2:" + conf.getDbPath();
         }
